7863e85395e1b0df987fa51dea98213aa20ce58e
akrambenali <akram1304@gmail.com>
2023-10-11 19:21:51 +0200

    update config

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
 
-const client_routes = require('./route/client')
-const cclient_routes = require('./route/cClient')
-const solution_routes = require('./route/solution')
-const search_routes = require('./route/search')
-const scores_routes = require('./route/scores')
+const client_routes = require("./route/client");
+const cclient_routes = require("./route/cClient");
+const solution_routes = require("./route/solution");
+const search_routes = require("./route/search");
+const scores_routes = require("./route/scores");
 
+require("dotenv").config();
 
-require('dotenv').config()
+// enabling CORS for some specific origins only. 
+let corsOptions = { 
+  origin : ['http://localhost:4200', 'https://app.atechor.com'], 
+} 
+ 
+app.use(cors(corsOptions)) 
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api/v1/clients', client_routes)
-app.use('/api/v1/cclients', cclient_routes)
-app.use('/api/v1/solutions', solution_routes)
-app.use('/api/v1/search', search_routes)
-app.use('/api/v1/scores', scores_routes)
+app.use(express.json());
+/* app.use(express.urlencoded({ extended: true })); */
+app.use("/api/v1/clients", client_routes);
+app.use("/api/v1/cclients", cclient_routes);
+app.use("/api/v1/solutions", solution_routes);
+app.use("/api/v1/search", search_routes);
+app.use("/api/v1/scores", scores_routes);
 
 // set port, listen for requests
 const PORT = process.env.PORT;
 const start = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     app.listen(PORT, () => console.log("Server started on port 5000"));
   } catch (error) {
     console.error(error);
@@ -36,5 +39,3 @@ const start = async () => {
 };
 
 start();
-
-
